ac4620755b6d6f5e3db5c3ab3ba1f781c4358d68
Edward <97937761+Edw4rd14@users.noreply.github.com>
2024-01-08 21:24:36 +0800

    Updated Main.py

--- Main.py
+++ Main.py
@@ -4,6 +4,10 @@
 # ADM NO: 2214407; 2239716
 
 # MAIN.PY
+'''
+This is the Main file which handles the main application, inclusive of the printing of the banner, handling user choices for the menu of the application,
+and running the Class methods to perform the menu option's functions.
+'''
 
 # IMPORT MODULES
 
@@ -16,21 +20,23 @@ class Main:
         self.choice = None
         self._menu_options = ['Add/Modify assignment statement', 'Display current assignment statements', 'Evaluate a single variable', 'Read assignment statements from file', 'Sort assignment statements', 'Exit']
         self.__options = {}
-        self._exit = "Bye, thanks for using ST1507 DSAA: Assignment Statement Evaluator & Sorter"
+        self._exit = "\nBye, thanks for using ST1507 DSAA: Assignment Statement Evaluator & Sorter"
 
     # Start banner
     def _start_banner(self):
+        # Print banner
         for order in self.__banner_order:
             print(self._banner[order])
-        # Print banner
-        # print(f'{self._banner[0]}\n{self._banner[1]}\n{self._banner[2]}\n{self._banner[3]}\n{self._banner[4]}\n{self._banner[5]}\n{self._banner[3]}\n{self._banner[0]}\n')
+        print("\n")
 
     # Menu prompt
     def _menu(self):
-        print("\n\nPlease select a choice ('1','2','3','4','5','6'):")
+        # Print menu
+        print("Please select a choice ('1','2','3','4','5','6'):")
         for i in range(len(self._menu_options)):
             print(f"    {i+1}. {self._menu_options[i]}")
-        self.choice = input("Enter choice: ")
+        # User input for menu option
+        self.choice = int(input("Enter choice: "))
 
     # Run main application
     def run(self):
@@ -54,18 +60,21 @@ class Main:
                     input("\nPlease enter key to continue...\n")
                 # Else if choice is 8 (exit application)
                 elif self.choice == 6:
-                    print(self._exit)
+                    print(f"{self._exit}")
                     break
                 # Else print error message (user option is not valid)
                 else:
-                    print("\nOnly options between 1 and 6 are available. Please try again.")
-            # Handle errors
-            except Exception as e:
-                print(e)
+                    print("Only options between 1 and 6 are available. Please try again.")
+            # Handle ValueError (user choice is not integer)
+            except ValueError:
+                print("\nInput must be an integer. Please try again.\n")
             # Handle Keyboard Interrupt (CRTL + C)
             except KeyboardInterrupt:
                 print(f"\n{self._exit}")
                 break
+            # Handle errors
+            except Exception as e:
+                print(e)
 
 # Instantiate Main class
 main = Main()
